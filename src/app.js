3e538d12c179462fc439b1530f23bc4a08ccd6fe
mariaperrone <perronemar148@gmail.com>
2021-08-16 19:30:40 +0000

    Ejercicio terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var random = Math.random() * 10;
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#exc1").innerHTML = generarExcusa();
+  });
+};
+
+var generarExcusa = () => {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let which = ["my homework", "my toe", "my car", "my shoe"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whichIndex = Math.floor(Math.random() * which.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+  return (
+    who[whoIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    which[whichIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
